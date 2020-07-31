558e95df0ddd1e25dde6d5bdfbe69a2efeb879ce
Ayren Jagar <ayrenjagar@gmail.com>
2020-07-31 10:09:35 +0200

    Challenge 1 Initial commit
    Challenge 1 Initial commit

---
+++ Challenge 1/Assets/Challenge 1/Scripts/PropellerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerControllerX : MonoBehaviour
+{
+    // Private variables
+    private float rotationSpeed = 150.0f;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get user's left/right input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Rotate the propellar at const speed when user moves the plane
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed * horizontalInput);
+    }
+}
